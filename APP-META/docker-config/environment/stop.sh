7e1aedd7488d0b82c18452770b9a152bcb1ba548
lili <lili@chainup.com>
2019-12-08 15:39:41 +0800

    up

--- APP-META/docker-config/environment/stop.sh
+++ APP-META/docker-config/environment/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -s --connect-timeout 3 localhost/offline
 sleep 10
-ps -ef|grep java|grep nls-domo |awk '{print $2}'|xargs kill -9 > /dev/null 2>&1
+ps -ef|grep java|grep spring-traning-base |awk '{print $2}'|xargs kill -9 > /dev/null 2>&1
