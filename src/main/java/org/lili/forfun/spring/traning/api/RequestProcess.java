97d032f92a75224103d907efa5c9fa2f722c600c
lili <lili@chainup.com>
2019-12-16 21:03:15 +0800

    up

--- src/main/java/org/lili/forfun/spring/traning/api/RequestProcess.java
+++ src/main/java/org/lili/forfun/spring/traning/api/RequestProcess.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lili
  * @date 2019/12/16 19:56
  * @description 查看从tomcat到springmvc请求的过程以及异常堆栈，以及拦截器，过滤器，事务，切面，
- * 重在分析web请求过程：1 初始化 2 请求过程
+ * 重在分析web请求过程：1 启动初始化 2 请求过程
  * https://blog.csdn.net/weixin_30856965/article/details/95026239
- * 里面有三大过程：1 servlet容器 2 spring容器 3 springmvc
+ * 里面有三大组件：1 servlet容器 2 spring容器 3 springmvc，有两大过程：1 启动 2 处理请求
  *
  */
 @Component
