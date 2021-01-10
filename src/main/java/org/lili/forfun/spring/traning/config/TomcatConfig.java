95fc8e167b65dcb3ff95b097b24fd166be29ce1a
lili <lili@chainup.com>
2021-01-10 14:11:37 +0800

    项目启动解决

--- src/main/java/org/lili/forfun/spring/traning/config/TomcatConfig.java
+++ src/main/java/org/lili/forfun/spring/traning/config/TomcatConfig.java
@@ -1,33 +1,49 @@
 package org.lili.forfun.spring.traning.config;
 
 import lombok.Data;
+import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.Http11NioProtocol;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ConfigurationProperties(prefix = "tom")
 @Data
 public class TomcatConfig {
-    private int maxConnections;
-    private int maxThreads;
-    private int soTimeout;
+
+    @Value("${port:8080}")
+    private int port;
 
     @Bean
-    public EmbeddedServletContainerFactory createEmbeddedServletContainerFactory() {
-        TomcatEmbeddedServletContainerFactory tomcatFactory = new TomcatEmbeddedServletContainerFactory();
-        tomcatFactory.addConnectorCustomizers((TomcatConnectorCustomizer)connector -> {
-            Http11NioProtocol protocol = (Http11NioProtocol)connector.getProtocolHandler();
-            protocol.setMaxConnections(getMaxConnections());
-            protocol.setMaxThreads(getMaxThreads());
-            protocol.setConnectionTimeout(getSoTimeout());
-            protocol.setMaxSwallowSize(-1);
-        });
-        return tomcatFactory;
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers(new ExchangeTomcatConnectionCustomizer());
+        return tomcat;
     }
+
+    /**
+     * springboot2.0.1以上
+     */
+    private class ExchangeTomcatConnectionCustomizer implements TomcatConnectorCustomizer {
+
+        private ExchangeTomcatConnectionCustomizer() {
+        }
+
+        @Override
+        public void customize(Connector connector) {
+            // tomcat connector 配置  http://tomcat.apache.org/tomcat-9.0-doc/config/http.html#Introduction
+            // 如果这里配置了port，conf.properties port将不生效
+            Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+            protocol.setPort(getPort());
+            protocol.setConnectionTimeout(30000);
+            protocol.setMaxThreads(2000);
+            protocol.setMaxConnections(2000);
+        }
+    }
+
+
 }
